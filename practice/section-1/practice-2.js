7ef5407e19e23596a5f387fc8439049d1a98a2e6
Amy oyzl <32362887+Amyoyzl@users.noreply.github.com>
2019-07-08 11:51:21 +0800

    edit js

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+    var n = 0;
+    var array = new Array();
+    for (var i = 0; i < collectionA.length; i++) {
+        for (var j = 0; j < collectionB.length; j++) {
+            for (var k = 0; k < collectionB[j].length; k++) {
+                if (collectionA[i] == collectionB[j][k]) {
+                    array[n++] = collectionB[j][k];
+                }
+            }
+        }
+    }
+    return array;
 }
--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+    var n = 0;
+    var array = new Array();
+    for (var i = 0; i < collectionA.length; i++) {
+        for (var j = 0; j < objectB.value.length; j++) {
+            if (collectionA[i] == objectB.value[j]) {
+                array[n++] = objectB.value[j];
+            }
+        }
+    }
+    return array;
 }
